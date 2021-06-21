6204b362fc1a537a3f03457ba16255f67ea70974
unknown <tejamacharla1997@gmail.com>
2021-06-21 21:32:56 +0530

    1datatype

---
+++ 1datatype.js
@@ -0,0 +1,145 @@
+String > "Hiii"  "Hello" 'Delhi' '3535' "353.35" 'true'
+Boolean > true false
+Number > 33 23543534 33234634646 34.44 .44
+
+null
+undefined
+
+////////es5///////
+Declare variable
+
+var a = 10
+var b = "hi"
+var c = true
+
+undefined
+typeof(a)
+"number"
+typeof(b)
+"string"
+typeof(c)
+"boolean"
+var d = "11"
+undefined
+var e = 'test'
+undefined
+typeof(d)
+"string"
+typeof(e)
+"string"
+var f = 10.2423
+undefined
+typeof(f)
+"number"
+
+///////////////
+
+var a = 20
+var b = 10
+
+a+b  (addition)
+30
+a-b
+10
+b-a
+-10
+a*b
+200
+a/b
+2
+a%b
+0
+
+4%2
+0
+4%3
+1
+
+
+2%4
+2
+3%4
+3
+4%4
+0
+5%2
+1
+5%3
+2
+modules to smaller to larger is always smaller
+
+/////////////////////
+var a = "hii"
+var b = "javaScript"
+
+var a = "hii"
+var b = "javaScript"
+a+b (concat)
+"hiijavaScript"
+
+a-b
+NaN (not a number)
+a*b
+NaN
+a/b
+NaN
+
+var a = "hi"
+undefined
+var b = 10
+undefined
+a+b
+"hi10"
+a-b
+NaN
+b+a
+"10hi"
+
+string+string = string
+string+number = string
+number+string = string
+number+number = number
+
+10+"20"+30
+"1020"+30
+"102030"
+
+10+20+"30"
+30+"30"
+"3030"
+
+"10"+20+30
+"1020"+30
+"102030"
+
+//////////////
+10+"20"+30-1
+"102030"-1
+102029
+
+10+20+"30"-1
+"3030"-1
+3029
+
+"10"+20+30-1
+"102030"-1
+102029
+
+"a"-1
+NaN
+"10"-1
+9
+"10"*2
+20
+"10"/2
+5
+"10a"-1
+NaN
+1-"2"
+-1
+"10"+1
+"101"
+"10"-"2"
+8
+"10"+"2"
+"102"
