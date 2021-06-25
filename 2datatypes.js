a1b666cc7a63ce97f630d4b73303a661a5861a63
unknown <tejamacharla1997@gmail.com>
2021-06-25 16:42:55 +0530

    2

---
+++ 2datatypes.js
@@ -0,0 +1,88 @@
+var a = true
+var b = true
+
+true+true
+2
+
+true+false
+1
+
+true is equal to 1
+false is equal to 0
+
+true+false
+1
+10+true
+11
+"a"+true
+"atrue"
+"a"+true+true
+"atruetrue"
+true+true+"a"
+"2a"
+
+10/false
+Infinity
+
+var age ="10"
+var out = age +10
+"1010"
+
+var a="10"
+var b="20"
+parseInt(a)+parseInt(b)
+30
+
+var c = "21.22"
+parseInt(c)
+21
+parseFloat(c)
+21.22
+parseFloat(c)+parseInt(a)
+31.22
+parseFloat(c)+parseFloat(a)
+31.22
+
+//es6
+
+Number(c)
+21.22
+Number(c)+Number(a)
+31.22
+
+var a ="10.22"
+parseInt(a)
+10
+
+var a ="10.52"
+parseInt(a)
+10
+
+var a = "Hi"
+parseInt(a)
+NaN
+
+var a = "11a2"
+var b = "12a"
+var c = "a12"
+
+parseInt(a)
+11
+parseInt(b)
+12
+parseInt(c)
+NaN
+
+
+var a = 10
+var b = "hi"
+var c = true
+
+isNaN(a) (is not a number)
+false
+
+isNaN(b)
+true
+
+isNaN(c)
+false
